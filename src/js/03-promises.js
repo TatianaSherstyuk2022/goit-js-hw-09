221f61e93dd8b878b9c24bf94e1c4a7fa54e90c9
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2023-01-09 18:38:09 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,21 +9,20 @@ formElem.addEventListener('submit', onSubmitForm);
 function onSubmitForm(e) {
   e.preventDefault();
 
-  const delay = Number(e.target.elements.delay.value);
+  let delay = Number(e.target.elements.delay.value);
   const step = Number(e.target.elements.step.value);
   const amount = Number(e.target.elements.amount.value);
 
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
       })
       .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
       });
     delay += step;
   }
-  console.log(daley, amount, step);
 }
 
 function createPromise(position, delay) {
@@ -38,6 +37,5 @@ function createPromise(position, delay) {
         reject(object);
       }
     }, delay);
-    console.log(daley, amount, step);
   });
 }
